b11211c6d8c4cb97d6a646280ac916b1e3d18b27
Pratik Kumar <pratikkumar791@gmail.com>
2024-03-22 18:19:42 +0530

    added emogies

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ function diceMe(){
     var playerOnePoint = pointGenerator();
     var playerTwoPoint = pointGenerator();
 
-    // console.log("Player 1 :"+playerOnePoint);
-    // console.log("Player 2: "+playerTwoPoint);
-
     document.querySelector(".img1").setAttribute("src","./images/dice"+playerOnePoint+".png");
     document.querySelector(".img2").setAttribute("src","./images/dice"+playerTwoPoint+".png");
 
 
     if(playerOnePoint === playerTwoPoint){
-        document.querySelector("h1").innerHTML= "Draw";
+        document.querySelector("h1").innerHTML= "📍Oh Its a Draw📍";
     }
     else if(playerOnePoint > playerTwoPoint){
-        document.querySelector("h1").innerHTML= "Player 1 Wins";
+        document.querySelector("h1").innerHTML= "🚩 Player 1 Wins";
     }
     else{
-        document.querySelector("h1").innerHTML= "Player 2 Wins";
+        document.querySelector("h1").innerHTML= "Player 2 Wins 🚩";
     }
 }
 
